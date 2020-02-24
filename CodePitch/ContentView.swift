ff4f21a2eeb0fbb09f437d65ae5e362bc2c87222
Anders Hovmöller <anders.hovmoller@trioptima.com>
2020-02-24 13:48:31 +0100

    Initial Commit

---
+++ CodePitch/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  CodePitch
+//
+//  Created by Anders Hovmöller on 2020-02-24.
+//  Copyright © 2020 Anders Hovmöller. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
